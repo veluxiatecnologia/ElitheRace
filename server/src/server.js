4c0f8b2e8795df85d070e45c74776a99d4a79b52
veluxiatecnologia <veluxiatecnoligia@gmail.com>
2026-01-10 11:21:10 -0300

    Fix: allow vercel previwes in cors

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,8 @@ const allowedOrigins = [
     'https://www.elitherace.com.br',
     'http://localhost:5173',
     'http://localhost:3000',
-    process.env.FRONTEND_URL
+    process.env.FRONTEND_URL,
+    /\.vercel\.app$/
 ].filter(Boolean);
 
 app.use(cors({
